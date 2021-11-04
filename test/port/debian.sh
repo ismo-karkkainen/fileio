b1a376b28421a92a01c19d0e5d61887722bfd19f
ismo-karkkainen <3896372+ismo-karkkainen@users.noreply.github.com>
2021-11-04 19:30:31 +0200

    Refresh package lists.

--- test/port/debian.sh
+++ test/port/debian.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 set -eu
+sudo apt-get update >/dev/null
 sudo apt-get install -y -q cmake make clang ruby libtiff-dev libpng-dev >/dev/null
 $1/test/port/clang-build.sh $1
--- test/port/sles.sh
+++ test/port/sles.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 set -eu
+sudo zypper refresh >/dev/null
 sudo zypper install -y cmake make gcc-c++ ruby libtiff-devel libpng-devel >/dev/null
 $1/test/port/gcc-build.sh $1
